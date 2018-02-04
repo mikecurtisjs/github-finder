6868d2c1f734fe9742bb267b81b89778f9714ee2
Michael Curtis <mikecurtisjs@gmail.com>
2018-02-03 19:45:18 -0800

    GitHub Users

---
+++ github.js
@@ -0,0 +1,25 @@
+
+
+class Github{
+  constructor(){
+    this.client_id = '5b1726c8be14a48aff77'
+    this.client_secret = 'c2aae0a801aaef8a93373deb2767e4ae2e05ad14'
+    this.repos_count = 5
+    this.repos_sort = 'created: asc'
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+
+    const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+
+
+    const profileData = await profileResponse.json()
+    const repos = await reposResponse.json()
+
+    return {
+      profile: profileData,
+      repos
+    }
+  }
+}
